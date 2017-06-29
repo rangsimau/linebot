09cf6b65bfbd2d0ffc306551a597752a7b27a523
rangsimau <rangsima.u@hotmail.com>
2017-06-29 20:20:18 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -83,27 +83,20 @@ if (!is_null($events['events'])) {
 		*/
 			$messages = [
 				  "type": "template",
-				  "altText": "this is a buttons template",
+				  "altText": "this is a confirm template",
 				  "template": {
-				      "type": "buttons",
-				      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-				      "title": "Menu",
-				      "text": "Please select",
+				      "type": "confirm",
+				      "text": "Are you sure?",
 				      "actions": [
 					  {
-					    "type": "postback",
-					    "label": "Buy",
-					    "data": "action=buy&itemid=123"
+					    "type": "message",
+					    "label": "Yes",
+					    "text": "yes"
 					  },
 					  {
-					    "type": "postback",
-					    "label": "Add to cart",
-					    "data": "action=add&itemid=123"
-					  },
-					  {
-					    "type": "uri",
-					    "label": "View detail",
-					    "uri": "http://example.com/page/123"
+					    "type": "message",
+					    "label": "No",
+					    "text": "no"
 					  }
 				      ]
 				  }
