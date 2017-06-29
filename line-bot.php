59ab131812d885cade2784d2e86f748f42cf4dfc
rangsimau <rangsima.u@hotmail.com>
2017-06-29 23:58:24 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -79,9 +79,19 @@ if (!is_null($events['events'])) {
 				//'text' => 'userid: '.$user.'\n roomid: '.$room .'\n groupid: '.$group 
 				//'text' => $start.' '.$text.' '.$end
 				
-				'type' => 'sticker',
-				'packageId' => '1',
-				'stickerId' => '1'
+				'type' => 'template',
+				'altText' => 'this is',
+				'template' => 
+				{
+					'type' => 'confirm',
+					'text' => 'Ary you?',
+					'actions' => 
+						[
+							'type' => 'message',
+							'label' => 'Yes',
+							'text' => 'userid'
+					]
+				}
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
