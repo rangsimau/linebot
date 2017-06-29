09f10ca9ff9856519be486bc345a1f28935bcb8c
rangsimau <rangsima.u@hotmail.com>
2017-06-29 19:32:37 +0700

    Update line-push.php

--- line-push.php
+++ line-push.php
@@ -1,20 +1,23 @@
-<?php
-$access_token = '8vKMiroG4T1TmRvFnAFu9VXRXp0WQJGPmyxAA4Ae5mx+NISTXeuv6B8fSiEj3Tu/IeNTVXuEHAokWIq3AayKY5GBSVCcalP/x3yh169JtnpZ2EfUg99oC2c3VcySEpyCDAuFXKSbXMip966sAUrqCAdB04t89/1O/w1cDnyilFU=';
-$userid = 'Ud392f1479ba3a4e92d82c98ba78e9f46';
-$groupid = 'C8b31f8f6b276cbc19262017f7ffe81e7';
+	<?php
+	$access_token = '8vKMiroG4T1TmRvFnAFu9VXRXp0WQJGPmyxAA4Ae5mx+NISTXeuv6B8fSiEj3Tu/IeNTVXuEHAokWIq3AayKY5GBSVCcalP/x3yh169JtnpZ2EfUg99oC2c3VcySEpyCDAuFXKSbXMip966sAUrqCAdB04t89/1O/w1cDnyilFU=';
+	$userid = 'Ud392f1479ba3a4e92d82c98ba78e9f46';
+	$groupid = 'C8b31f8f6b276cbc19262017f7ffe81e7';
 
-$type = $_GET['t'];
-$text = $_GET['s'];
-      
-			$end = '•';
-			$start = '•';
-if($type == 'user'){$to = $userid;}
-else($type == 'group'){$to = $groupid;}
+	$type = $_GET['t'];
+	$text = $_GET['s'];
+
+	$end = '•';
+	$start = '•';
+	if($type == 'user'){
+		$to = $userid;
+	}else($type == 'group'){
+		$to = $groupid;
+	}
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				//'text' => 'userid: '.$user.'\n roomid: '.$room .'\n groupid: '.$group 
-				'text' => $start.$text.$end;
+				'text' => $start.$text.$end
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
