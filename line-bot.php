365be2d59c50e55c7475790a398774f134039230
rangsimau <rangsima.u@hotmail.com>
2017-06-30 00:09:50 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -74,7 +74,7 @@ if (!is_null($events['events'])) {
 	}elseif($type=='room'){
 		$id = $roomid;
 	}
-			$messages = [
+			/*$messages = [
 				//'type' => 'text',
 				//'text' => 'userid: '.$user.'\n roomid: '.$room .'\n groupid: '.$group 
 				//'text' => $start.' '.$text.' '.$end
@@ -86,7 +86,8 @@ if (!is_null($events['events'])) {
 					'text' => 'Are?',
 					'actions' => []
 				}
-			];
+			];*/
+			echo 'pass';
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
