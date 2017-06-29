cfa98aac82e2c7ef980944b3113157286330ef21
rangsimau <rangsima.u@hotmail.com>
2017-06-29 20:27:33 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -74,33 +74,33 @@ if (!is_null($events['events'])) {
 		$id = $userid;
 	}
 
-			
+		/*	
 			$messages = [
 				'type' => 'text',
 				//'text' => 'userid: '.$user.'\n roomid: '.$room .'\n groupid: '.$group 
 				'text' => $start.' '.$text.' '.$end
 			];
-		
-		/*	$messages = [
-				  "type": "template",
-				  "altText": "this is a confirm template",
-				  "template": {
-				      "type": "confirm",
-				      "text": "Are you sure?",
-				      "actions": [
+		*/
+			$messages = [
+				  "type" => "template",
+				  "altText" => "this is a confirm template",
+				  "template" => {
+				      "type" => "confirm",
+				      "text" => "Are you sure?",
+				      "actions"=> [
 					  {
-					    "type": "message",
-					    "label": "Yes",
-					    "text": "yes"
+					    "type" => "message",
+					    "label"=> "Yes",
+					    "text"=> "yes"
 					  },
 					  {
-					    "type": "message",
-					    "label": "No",
-					    "text": "no"
+					    "type"=> "message",
+					    "label"=> "No",
+					    "text"=> "no"
 					  }
 				      ]
 				  }
-				];*/
+				];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
