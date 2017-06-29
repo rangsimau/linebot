e9a597eed156011463cc593205f3ca0d4f6e461d
rangsimau <rangsima.u@hotmail.com>
2017-06-29 23:50:10 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -75,9 +75,23 @@ if (!is_null($events['events'])) {
 		$id = $roomid;
 	}
 			$messages = [
-				'type' => 'text',
+				//'type' => 'text',
 				//'text' => 'userid: '.$user.'\n roomid: '.$room .'\n groupid: '.$group 
-				'text' => $start.' '.$text.' '.$end
+				//'text' => $start.' '.$text.' '.$end
+				
+				'type' => 'template',
+				'altText' => 'Bright Test',
+				'template' => [
+					'type' => 'confirm',
+					'text' => 'Are you sure?',
+					'actions' => [{
+						'type' => 'message',
+						'label' => 'Yes',
+						'text' => 'yes'},{
+						'type' => 'message',
+						'label' => 'No',
+						'text' => 'no'}]
+				]
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
