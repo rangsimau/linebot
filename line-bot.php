682494907fb93e779dc49462c2581e240f753839
rangsimau <rangsima.u@hotmail.com>
2017-06-29 18:31:56 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -24,7 +24,7 @@ $events = json_decode($content, true);
 			//	$text = $gettext;}
 
 			// Build message to reply back
-			if(!is_null($var) && $var != ''){
+			//if(!is_null($var) && $var != ''){
 			$messages = [
 				'type' => 'text',
 				//'text' => 'userid: '.$user.' roomid: '.$room .' groupid: '.$group 
@@ -50,7 +50,7 @@ $events = json_decode($content, true);
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			}
+			//}
 		//}
 	//}
 //}
