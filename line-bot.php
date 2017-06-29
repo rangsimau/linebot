438767d0ce0adeb0b40ab73c59ccbf854db734b3
rangsimau <rangsima.u@hotmail.com>
2017-06-29 08:56:55 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -12,17 +12,22 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$gettext = $event['message']['text'];
 			$user = $event['source']['userId'];
 			$room = $event['source']['roomId'];
 			$group = $event['source']['groupId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			if($text == 'leave'){
+				$text = 'Bye';
+			}else{
+				$text = $gettext;}
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'userid: '.$user.' roomid: '.$room .' groupid: '.$group 
+				//'text' => 'userid: '.$user.' roomid: '.$room .' groupid: '.$group 
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,6 +49,21 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			
+			if($gettext == 'leave'){
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/room/'.$group.'/leave';
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			}
 		}
 	}
 }
