193baa6603ef0df866cfde700b21d9f584f66865
rangsimau <rangsima.u@hotmail.com>
2017-06-29 08:39:34 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $messages,
+				'messages' => "Thank You",
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
