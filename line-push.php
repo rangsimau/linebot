e6d8d4465350509e786c1a370f1448e7706fd55b
rangsimau <rangsima.u@hotmail.com>
2017-06-29 19:37:38 +0700

    Update line-push.php

--- line-push.php
+++ line-push.php
@@ -1,4 +1,5 @@
 	<?php
+	echo '0';
 	$access_token = '8vKMiroG4T1TmRvFnAFu9VXRXp0WQJGPmyxAA4Ae5mx+NISTXeuv6B8fSiEj3Tu/IeNTVXuEHAokWIq3AayKY5GBSVCcalP/x3yh169JtnpZ2EfUg99oC2c3VcySEpyCDAuFXKSbXMip966sAUrqCAdB04t89/1O/w1cDnyilFU=';
 	$userid = 'Ud392f1479ba3a4e92d82c98ba78e9f46';
 	$groupid = 'C8b31f8f6b276cbc19262017f7ffe81e7';
