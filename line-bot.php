5204cb489207b34d3f5282316fb347bb05aeed54
rangsimau <rangsima.u@hotmail.com>
2017-06-29 18:45:12 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -35,9 +35,9 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/push';
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'to' => 'Ud392f1479ba3a4e92d82c98ba78e9f46',
+				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
