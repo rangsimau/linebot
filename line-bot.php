1174a0f1ff076bbbc95024a57fb24ac04fa62d89
rangsimau <rangsima.u@hotmail.com>
2017-06-29 09:06:59 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -49,21 +49,6 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			
-			if($gettext == 'leave'){
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/room/'.$group.'/leave';
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-			}
 		}
 	}
 }
