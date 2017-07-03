598c4008518d081166e537e6eebc2fe1c40cbadd
rangsimau <rangsima.u@hotmail.com>
2017-07-03 22:08:07 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 				
 			}elseif(substr($gettext,0,6) == '@price'){
 				$bike = urlencode(substr($gettext,7));
-				$text = file_get_contents('http://rangsima.com/bikeprice.php?bike='.$bike); 
+				$text = 'http://rangsima.com/bikeprice.php?bike='.$bike; 
 			}elseif($gettext == '@payment check'){
 				$text = 'under construction..';
 			}elseif($gettext == '@payment fix'){
