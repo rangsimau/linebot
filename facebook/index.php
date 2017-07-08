1b75a57cccee0b8e1583476318f8950844932564
rangsimau <rangsima.u@hotmail.com>
2017-07-09 00:27:34 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -3,13 +3,12 @@ echo $_GET['hub_challenge'];
 
 $content=file_get_contents('php://input');
 $events = json_decode($content, true);
-
-$item_type = $events['entry'][0]['changes'][0]['value']['item'];
+$item = $events['entry'][0]['changes'][0]['value']['item'];
 $sender_id = $events['entry'][0]['changes'][0]['value']['sender_id'];
 $verb = $events['entry'][0]['changes'][0]['value']['verb'];
 //if($sender_id == '236113873116564' && $verb == 'add'){
 //  if($item_type == 'status' || $item_type == 'video' || $item_type == 'photo'){
-    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$events['entry'][0]['changes'][0]['value']['item']);
+    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$item);
 //  }else{
    // $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$item_type.' from tp');
 //}else{
