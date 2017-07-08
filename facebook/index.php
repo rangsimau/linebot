8f7e2262d38b9633aff1ac86c9039908db34fce7
rangsimau <rangsima.u@hotmail.com>
2017-07-09 00:19:51 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -4,10 +4,14 @@ echo $_GET['hub_challenge'];
 $content=file_get_contents('php://input');
 $events = json_decode($content, true);
 
-foreach($events['entry'][0]['changes'] as $entry){
-  $field = $entry['field'];
-}
-  
-$get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$field.urlencode($content));
-
+$item_type = $events['entry'][0]['changes'][0]['value']['item'];
+$sender_id = $events['entry'][0]['changes'][0]['value']['sender_id'];
+$verb = $events['entry'][0]['changes'][0]['value']['verb'];
+if($sender_id == '236113873116564' && $verb == 'add'){
+  if($item_type == 'status' || $item_type == 'video' || $item_type == 'photo'){
+    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$item_type.' from tp');
+  }else{
+    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$item_type.' from tp');
+}else{
+    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg=else');
 ?>
