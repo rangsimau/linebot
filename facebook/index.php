d967b3ced82c744032354785269b73e3628281be
rangsimau <rangsima.u@hotmail.com>
2017-07-08 20:41:45 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -2,7 +2,7 @@
 echo $_GET['hub_challenge']; 
 
 $content=file_get_contents('php://input');
-$events = json_decode($content, true);
-$get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$events['entry'][0]['time']);
+//$events = json_decode($content, true);
+$get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($content));
 
 ?>
