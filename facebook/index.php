9de8559bd93d148f7c4ec933f12eb809bad1b843
rangsimau <rangsima.u@hotmail.com>
2017-07-09 00:37:02 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -5,13 +5,16 @@ $content=file_get_contents('php://input');
 $events = json_decode($content, true);
 $item = $events['entry'][0]['changes'][0]['value']['item'];
 $sender_name = $events['entry'][0]['changes'][0]['value']['sender_name'];
+$sender_id = $events['entry'][0]['changes'][0]['value']['sender_id'];
 $verb = $events['entry'][0]['changes'][0]['value']['verb'];
-//if($sender_id == '236113873116564' && $verb == 'add'){
-if($item == 'status' || $item == 'video' || $item == 'photo'){
-    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($item.' '.$sender_name));
-//  }else{
-   // $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.$item_type.' from tp');
+if($sender_id == '236113873116564' && $verb == 'add'){
+    if($item == 'status' || $item == 'video' || $item == 'photo'){
+        $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($item.' '.$sender_name));
+    }else{
+        $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($item.' '.$sender_name));
+    }
 }else{
-   $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg=else');
+    $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg=.urlencode($item.' '.$sender_name));
 }
+
 ?>
