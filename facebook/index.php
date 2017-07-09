11ce16278d037f49d71b77b1f5849de1fd621017
rangsimau <rangsima.u@hotmail.com>
2017-07-09 21:53:25 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -8,11 +8,11 @@ $sender_name = $events['entry'][0]['changes'][0]['value']['sender_name'];
 $sender_id = $events['entry'][0]['changes'][0]['value']['sender_id'];
 $verb = $events['entry'][0]['changes'][0]['value']['verb'];
 if($sender_id == '236113873116564' && $verb == 'add'){
-    //if($item == 'status' || $item == 'video' || $item == 'photo'){
+    if($item == 'status' || $item == 'video' || $item == 'photo'){
         $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($sender_name.' '. $verb.' '.$item));
-    //}
+    }
 }else{
-   //$get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg=else');
+   $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($sender_name.' '.$verb));
 }
 
 ?>
