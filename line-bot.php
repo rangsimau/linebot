550e30f158afbcf3255ce043d530d33101b92c27
rangsimau <rangsima.u@hotmail.com>
2017-07-10 07:55:19 +0700

    Update line-bot.php

--- line-bot.php
+++ line-bot.php
@@ -8,7 +8,7 @@ $events = json_decode($content, true);
 $end = '•';
 $start = '•';
 $osposurl = 'rangsima.com';
-$mainurl = 'rangsima.com/tpmotorcycle.com';
+$mainurl = 'tpmotorcycle.com';
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
