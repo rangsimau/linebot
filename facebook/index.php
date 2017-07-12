5e1d907cff1d7b57ec0d03d05fbe45a3c57961a0
rangsimau <rangsima.u@hotmail.com>
2017-07-12 17:25:01 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -19,11 +19,11 @@ if($sender_id == '236113873116564' && $verb == 'add'){
     if($field == 'conversations'){
         $access_token = 'EAACquzrGeZCYBABhoDtgtgMMEdWUc7rbdeHT3w4jQlD8ObPPA8HL09rHsakUPnAGElRTCGhvL5jHmK68VLoRZBLwlR5mFK9XRrEg7B9orsy4S3xkXeKXREOI0ZCi6yQL29uYD6qOyaJkdX9BZC3iv4HtuqeoMZBKf440EtlV3bRTqqXuUuXHxtEsAHsVYfEZAQybYARu5aEwZDZD';
         $thread_id = $events['entry'][0]['changes'][0]['value']['thread_id'];
-        $face_msg = @file_get_contents('https://graph.facebook.com/'.$thread_id.'?fields=messages{message,from}&access_token='.$access_token);
-        $face = json_decode($face_msg, true);
+        $face_msg = file_get_contents('https://graph.facebook.com/'.$thread_id.'?fields=messages{message,from}&access_token='.$access_token);
+        //$face = json_decode($face_msg, true);
         $msg = $face['messages']['data'][0]['message'];
         $from = $face['messages']['data'][0]['from']['name'];
-        $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($from.' said: '.$msg));
+        $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($face_msg));
     }
    //$get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($sender_name.' '.$verb));
 }
