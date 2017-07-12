e0cb088c8431df628b7ef9b577d084ac874c82c3
rangsimau <rangsima.u@hotmail.com>
2017-07-12 08:55:53 +0700

    Update index.php

--- facebook/index.php
+++ facebook/index.php
@@ -4,6 +4,7 @@ echo $_GET['hub_challenge'];
 $content=file_get_contents('php://input');
 $events = json_decode($content, true);
 $item = $events['entry'][0]['changes'][0]['value']['item'];
+$field = $events['entry'][0]['changes'][0]['field'];
 $sender_name = $events['entry'][0]['changes'][0]['value']['sender_name'];
 $sender_id = $events['entry'][0]['changes'][0]['value']['sender_id'];
 $verb = $events['entry'][0]['changes'][0]['value']['verb'];
@@ -15,6 +16,9 @@ if($sender_id == '236113873116564' && $verb == 'add'){
         $update = file_get_contents('http://tpmotorcycle.com/picture/update_facebook.php?postid='.urlencode($post_id));
     }
 }else{
+    if($field = 'conversations'){
+        $get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($content));
+    }
    //$get = file_get_contents('https://still-thicket-82675.herokuapp.com/line-bot.php?msg='.urlencode($sender_name.' '.$verb));
 }
 
